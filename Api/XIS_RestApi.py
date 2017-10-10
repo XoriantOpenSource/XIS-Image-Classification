6c5af9e97949d777bd95360daa3a436561374101
sujit25 <Sujit.Nalawade@Xoriant.com>
2017-10-10 14:03:08 +0530

    rest api for image labels completed

--- Api/XIS_RestApi.py
+++ Api/XIS_RestApi.py
@@ -1,76 +1,83 @@
-from flask import Flask,request, make_response,jsonify,abort
+from flask import Flask, request, make_response, jsonify, abort
+import json
 from AppConfig.AppConfigSettings import AppConfigSettings
 from Mongodb_OpsHandler.DbOperationsHandler import DbOperationsHandler
-import json
-from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
-api = Api(app)
 
 
-class Initilizer:
-    def __init__(self):
-        self.configManager = None
-        self.dbOpsManager = None
-        self.initializationSuccessful = False
-
-    def initialize(self):
-        self.configManager = AppConfigSettings()
-        if self.configManager.read_config_settings():
-            self.dbOpsManager =self.dbOpsManager(self.configManager)
-            self.initializationSuccessful = True
-        else:
-            self.initializationSuccessful = False
-
-
-class Category(Resource):
-    def __init__(self):
-        self.initializer = Initilizer()
-        if not self.initializer.initializationSuccessful:
-            self.initializer.initialize()
-
-    def get(self):
-        jsonify(['animals','plants','buildings'])
-
-
-class Label(Resource):
-    def __init__(self):
-        self.initializer = Initilizer()
-        if not self.initializer.initializationSuccessful:
-            self.initializer.initialize()
-
-        self.requestParser = reqparse.RequestParser()
-        self.requestParser.add_argument('query',type='list',required=True,location='json')
-        super(Label, self).__init__()
-
-    def post(self):
-        args = self.requestParser.parse_args()
-        print(args)
-        if len(args) == 0 or args is None:
-            return jsonify({"result":"No images found"})
-
-        return
-
-api.add_resource(Category, "/xis/api/v1.0/category", endpoint='category')
-api.add_resource(Label, '/xis/api/v1.0/label', endpoint='label')
-
-
-# def init_api():
-#     global config_settings,dbOpsHandler
-#     config_settings= AppConfigSettings()
-#     if config_settings.read_config_settings():
-#         dbOpsHandler = DbOperationsHandler(config_settings)
+# api = Api(app)
 #
-# @app.route("/xis/api/v1.0/labels",methods=['POST'])
-# def get_categories():
 #
-#     image_query = request.get_json()
-#     if image_query is None or len(image_query) == 0:
-#         return abort(400)
-#     print(image_query['query'])
-#     # result =jsonify(dbOpsHandler.search_by_labels(query_array))
-#     # print(result)
-#     # return result
+# class Initilizer:
+#     def __init__(self):
+#         self.configManager = None
+#         self.dbOpsManager = None
+#         self.initializationSuccessful = False
+#
+#     def initialize(self):
+#         self.configManager = AppConfigSettings()
+#         if self.configManager.read_config_settings():
+#             self.dbOpsManager =self.dbOpsManager(self.configManager)
+#             self.initializationSuccessful = True
+#         else:
+#             self.initializationSuccessful = False
+#
+#
+# class Category(Resource):
+#     def __init__(self):
+#         # self.initializer = Initilizer()
+#         # if not self.initializer.initializationSuccessful:
+#         #     self.initializer.initialize()
+#         super(Category, self).__init__()
+#     def get(self):
+#         return make_response(jsonify(['animals','plants','buildings']))
+#
+#
+# class Label(Resource):
+#     def __init__(self):
+#         # self.initializer = Initilizer()
+#         # if not self.initializer.initializationSuccessful:
+#         #     self.initializer.initialize()
+#         super(Label, self).__init__()
+#         self.requestParser = reqparse.RequestParser()
+#         self.requestParser.add_argument('query',type='list',required=True,location='json')
+#         super(Label, self).__init__()
+#
+#     def post(self):
+#         #return make_response(jsonify({"message":" Label's post hit"}))
+#         args = self.requestParser.parse_args()['query']
+#         print(args)
+#         if len(args) == 0 or args is None:
+#             return make_response(jsonify({"result":"No images found"}))
+#         else:
+#             return make_response(jsonify({"message": " Label's post hit"}))
+#
+#
+# api.add_resource(Category, "/xis/api/v1.0/category", endpoint='category')
+# api.add_resource(Label, '/xis/api/v1.0/label', endpoint='label')
+
+
+def init_api():
+    global config_settings, dbOpsHandler
+    config_settings = AppConfigSettings()
+    if config_settings.read_config_settings():
+        dbOpsHandler = DbOperationsHandler(config_settings)
+
+
+@app.route("/xis/api/v1.0/label", methods=['POST'])
+def get_categories():
+    image_query = request.get_json()
+    if image_query is None or len(image_query) == 0:
+        return abort(400)
+    search_query = image_query['query']
+    result = dbOpsHandler.search_by_labels(search_query)
+    jsonResponse = json.dumps(result)
+    if len(result) == 0 or result is None:
+        return make_response(jsonify({"status":"failure","result": "[]"})), 404
+    else:
+        return make_response(jsonify({"status":"success","result": jsonResponse})), 200
 
 if __name__ == "__main__":
-    app.run()
+    init_api()
+    app.run(debug=True)
