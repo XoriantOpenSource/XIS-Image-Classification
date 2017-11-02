986a77ea9180835a46ebd58dabf8c04d4e237fc3
pandey_ak <Akash.Pandey@Xoriant.com>
2017-11-02 11:12:54 +0530

    arguments

--- XIS_Image_Indexer/__main__.py
+++ XIS_Image_Indexer/__main__.py
@@ -1,13 +1,18 @@
 from AppConfig.ConfigSettings import ConfigSettings
 from XIS_Image_Indexer.ImageIndexer.Image_Indexer import ImageIndexer
-
+from argparse import ArgumentParser
 
 if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('-s', '--source_dir', help='Directory containing images to be indexed')
+    parser.add_argument('-t', '--target_dir', help='Directory where images may be downloaded')
+    args = parser.parse_args()
+    print(args.source_dir, args.target_dir)
     config_settings = ConfigSettings()
     if not config_settings.read_config_settings():
         print("error occurred while reading config")
-    config_settings.source_images_location = input("Directory to pick images from => ")
-    config_settings.download_location = input("Directory where images may be downloaded => ")
+    config_settings.source_images_location = args.source_dir
+    config_settings.download_location = args.target_dir
     image_indexer = ImageIndexer(config_settings)
 
     # if run in index mode
@@ -17,4 +22,3 @@ if __name__ == '__main__':
     # query = ['cat','dog']
     # image_indexer.search_by_query(query)
     # return
-
